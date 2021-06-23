00083c3254ee051772e611dfb6365f6a01041b28
zhangjunliang <779581051@qq.com>
2021-06-23 21:17:25 +0800

    备注

--- run.py
+++ run.py
@@ -8,7 +8,7 @@ parser = argparse.ArgumentParser(description='run east')
 parser.add_argument('-o', type=str, required=True, help='obj')
 parser.add_argument('-f', type=str, required=True, default='', help='fun')
 parser.add_argument('-p', default=None, help='params')
-parser.add_argument('-t', type=int, default=0, help='params')
+parser.add_argument('-t', type=int, default=0, help='jump')
 
 args = parser.parse_args()
 
